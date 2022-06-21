7288700c4f14afed238b053f2da2fdb55aa65652
shmvsky <grrenshi1337@gmail.com>
2022-06-21 12:34:35 +0300

    Spring Initializr template

---
+++ src/main/java/ru/shmvsky/springchat/SpringChatApplication.java
@@ -0,0 +1,13 @@
+package ru.shmvsky.springchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shmvsky/springchat/SpringChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shmvsky.springchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
